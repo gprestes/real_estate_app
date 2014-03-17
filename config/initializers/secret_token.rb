629a77dd416ea17a6656a735a6376a3e7458645c
Guilherme <gprestes@thoughtworks.com>
2014-03-16 22:51:07 -0700

    RSpec and Gemfile update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,28 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RealEstateApp::Application.config.secret_token = '3d04de8b617bd7b7a39b4cead7fd546773c5b34f1148e5fbd3fe39fa1e3f9a37d3c71f9a30597c10334fb1b5eb555ce91b3b949147dc9278437f0dc15a877eda'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+RealEstateApp::Application.config.secret_key_base = secure_token
+
