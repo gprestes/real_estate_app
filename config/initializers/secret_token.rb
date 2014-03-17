e10f2689134c557cb49df9735b9df5fa959051c1
Guilherme <gprestes@thoughtworks.com>
2014-03-16 22:12:53 -0700

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RealEstateApp::Application.config.secret_token = '3d04de8b617bd7b7a39b4cead7fd546773c5b34f1148e5fbd3fe39fa1e3f9a37d3c71f9a30597c10334fb1b5eb555ce91b3b949147dc9278437f0dc15a877eda'
